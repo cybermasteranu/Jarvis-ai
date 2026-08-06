881a3ced3fbf0b5dd116c0da72a2e7f181d84610
cybermasteranu <cybermasteranu@gmail.com>
2026-08-06 23:35:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function startJarvis() {
 
+    const message = new SpeechSynthesisUtterance();
+
+    message.text = "Hello Anu. JARVIS is online. How can I help you?";
+
+    message.rate = 1;
+    message.pitch = 1;
+
+    window.speechSynthesis.speak(message);
+
+}
